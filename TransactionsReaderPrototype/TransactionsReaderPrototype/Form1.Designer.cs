be0cafdc9e3a3f1413f313ec12a30ff66f41b17f
Ibrahim <ibrahim.ragab.net@gmail.com>
2018-04-20 22:41:19 +0200

    51

---
+++ TransactionsReaderPrototype/TransactionsReaderPrototype/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TransactionsReaderPrototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectPdf = new System.Windows.Forms.Button();
+            this.StartProcessing_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PdfFile_txt = new System.Windows.Forms.TextBox();
+            this.outputFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.openPdfDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // SelectPdf
+            // 
+            this.SelectPdf.Location = new System.Drawing.Point(465, 40);
+            this.SelectPdf.Name = "SelectPdf";
+            this.SelectPdf.Size = new System.Drawing.Size(44, 34);
+            this.SelectPdf.TabIndex = 16;
+            this.SelectPdf.Text = "...";
+            this.SelectPdf.UseVisualStyleBackColor = true;
+            // 
+            // StartProcessing_btn
+            // 
+            this.StartProcessing_btn.Location = new System.Drawing.Point(55, 111);
+            this.StartProcessing_btn.Name = "StartProcessing_btn";
+            this.StartProcessing_btn.Size = new System.Drawing.Size(144, 45);
+            this.StartProcessing_btn.TabIndex = 15;
+            this.StartProcessing_btn.Text = "Generate CSV";
+            this.StartProcessing_btn.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "PDF File";
+            // 
+            // PdfFile_txt
+            // 
+            this.PdfFile_txt.Location = new System.Drawing.Point(131, 46);
+            this.PdfFile_txt.Name = "PdfFile_txt";
+            this.PdfFile_txt.Size = new System.Drawing.Size(328, 22);
+            this.PdfFile_txt.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 323);
+            this.Controls.Add(this.SelectPdf);
+            this.Controls.Add(this.StartProcessing_btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PdfFile_txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button SelectPdf;
+        private System.Windows.Forms.Button StartProcessing_btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PdfFile_txt;
+        private System.Windows.Forms.FolderBrowserDialog outputFolderBrowserDialog;
+        private System.Windows.Forms.OpenFileDialog openPdfDialog;
+    }
+}
+
---
+++ TransactionsReaderPrototype/TransactionsReaderPrototype/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TransactionsReaderPrototype
+{
+    public partial class Form1 : Form
+    {
+        private string _outoutDir;
+        private string _inputPdfFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+            openPdfDialog.Filter = "PDF files (*.pdf)|*.pdf";
+            Shown += Form1_Shown;
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            SelectPdf.Click += LoadPdfFile;
+            StartProcessing_btn.Click += StartProcessing_btn_Click;
+        }
+
+        private void StartProcessing_btn_Click(object sender, EventArgs e)
+        {
+            var tetxtFile = ConvertPDFToText();
+            ConvertTextToCsv(tetxtFile);
+        }
+
+        private void ConvertTextToCsv(string tetxtFile)
+        {
+            var outputCsv = $"{_inputPdfFile}.CSV";
+            var header = "DATA CONT.; DATA VALUTA; DATA DISP.; A VOSTRO DEBITO; A VOSTRO CREDITO; DESCRIZIONE DELLE OPERAZIONI;\n";
+            var lines =new List<string>();
+            File.AppendAllText(outputCsv,header);
+            var regexParser = new Regex(@"^((\s+)(?<dates>((\d{2}[/]\d{2}[/]\d{2})\s+){3})(?<number>((\d{1,3}\.*)+\,\d{2})[-]?))(?<message>.*)$");
+            foreach (var line in File.ReadLines(tetxtFile))
+            {
+                var match = regexParser.Match(line);
+                var suc = match.Success;
+                if (suc)
+                {
+                    var dates = match.Groups["dates"].Value;
+                    var splitedDate = dates.Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+                    var number = match.Groups["number"].Value;
+                    var numberRepresentation = "";
+                    numberRepresentation = number.EndsWith("-") ? $"{number};{string.Empty}" : $"{string.Empty};{number}";
+                    var message = match.Groups["message"].Value;
+                    var lineToAdd =
+                        $"{splitedDate[0]};{splitedDate[1]};{splitedDate[2]};{numberRepresentation};{message}";
+                    lines.Add(lineToAdd);
+                }
+            }
+            File.AppendAllLines(outputCsv,lines);
+            MessageBox.Show("Operation done please open the generated file to check.","DONE");
+        }
+
+        private string ConvertPDFToText()
+        {
+            var outputFile = Path.GetTempFileName();
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "tools\\pdftotext.exe",
+                Arguments = $"\"{_inputPdfFile}\" \"{outputFile}\" -r 100 -layout",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process processTemp = new Process
+            {
+                StartInfo = startInfo,
+                EnableRaisingEvents = true
+            };
+            try
+            {
+                processTemp.Start();
+                processTemp.WaitForExit();
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            return outputFile;
+        }
+
+        private void LoadPdfFile(object sender, EventArgs e)
+        {
+            var openDialogResult = openPdfDialog.ShowDialog();
+            if (openDialogResult == DialogResult.OK)
+            {
+                _inputPdfFile = openPdfDialog.FileName;
+                PdfFile_txt.Text = _inputPdfFile;
+            }
+        }
+    }
+}
